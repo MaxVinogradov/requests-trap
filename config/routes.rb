19a53d0629231883a25fcaf77b46b759a6858f1a
MaxVinogradov <vinogradov.max49@gmail.com>
2017-09-18 23:03:24 +0300

    Views and correct routers has been added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  match ':trap_id' => 'trap#trap', via: [:all]
-  match ':trap_id/requests' => 'requests#requests', via: [:all]
+  match ':trap_id' => 'trap#visit', via: [:get]
+  match ':trap_id/requests' => 'requests#statistic', via: [:all]
 
 end
