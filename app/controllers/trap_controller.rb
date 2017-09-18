9e3008f77053ab56c23d9ccfdc1f5b16db39a51c
MaxVinogradov <vinogradov.max49@gmail.com>
2017-09-19 01:16:41 +0300

    Initial logic of working with database.

--- app/controllers/trap_controller.rb
+++ app/controllers/trap_controller.rb
@@ -1,7 +1,14 @@
 class TrapController < ApplicationController
 
   def visit
-    logger.info 'Visited: ' + params['trap_id'].inspect
+    logger.info 'Visited: ' + params['trap_id'].to_s
+    RequestItem.new(
+        trap_id: params['trap_id'].to_s,
+        r_date: Time.now,
+        r_data: request.headers.env.select {
+            |k, _| k.in?(ActionDispatch::Http::Headers::CGI_VARIABLES) || k =~ /^HTTP_/
+        }
+    ).save
   end
 
 end
