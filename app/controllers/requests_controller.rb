6efdc402cced4e957372450ecfa11ab14f802e2b
MaxVinogradov <vinogradov.max49@gmail.com>
2017-09-20 03:36:38 +0300

    Redundant MVC view-controller has been removed.

--- app/controllers/requests_controller.rb
+++
@@ -1,5 +0,0 @@
-class RequestsController < ApplicationController
-  def statistic
-    logger.info 'Statistic for trap: ' + params['trap_id'].inspect
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  resource :trap_id
   root 'welcome#index'
 
   match ':trap_id' => 'trap#visit', via: [:all]
   match ':trap_id/requests' => 'statistic#index', via: [:all]
-  match ':trap_id/requests2' => 'requests#statistic', via: [:all]
 
-  # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
 
 end
