e4493aebdc24dce6dd1edb1d05a069526ca00f5c
MaxVinogradov <vinogradov.max49@gmail.com>
2017-09-24 17:25:57 +0300

    [#4] Routing has been fixed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  match ':trap_id' => 'trap#visit', via: [:all]
   match ':trap_id/requests' => 'statistic#visit', via: [:all]
+  match ':trap_id(/*other)' => 'trap#visit', via: [:all]
+  # TODO: ask about case: ':trap_id/requests/*other'
 
   mount ActionCable.server => '/cable'
-
 end
