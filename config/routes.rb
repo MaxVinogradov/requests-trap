e3cb10f27322745cde1600fc0044bdbd71c7ebd4
Maxim Vinogradov <vinogradov.max97@gmail.com>
2017-09-25 09:34:11 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  match ':trap_id/requests' => 'statistic#visit', via: [:all]
+  match ':trap_id/requests' => 'statistic#visit', via: [:get]
   match ':trap_id(/*other)' => 'trap#visit', via: [:all]
   # TODO: ask about case: ':trap_id/requests/*other'
 
